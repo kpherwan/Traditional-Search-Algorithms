7614825e57845f3f5c689cce1c972f6335f3b5e3
KPherwani <karishma.pherwani21@gmail.com>
2021-02-10 11:48:36 -0800

    refactoring

--- src/work/homework.java
+++ src/work/homework.java
@@ -5,24 +5,35 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-class Coordinate implements Comparable<Coordinate> {
-    int x;
-    int y;
-    Coordinate parent;
-    int priority;
+class Node implements Comparable<Node> {
+    public int x;
+    public int y;
+    public Node parent;
+    public int pathCost;
+    public int gCost;
+    public int hCost;
 
-    Coordinate(int x, int y, Coordinate parent) {
+    Node(int x, int y, Node parent) {
         this.x = x;
         this.y = y;
         this.parent = parent;
-        this.priority = 0;
+        this.pathCost = 0;
     }
 
-    Coordinate(int x, int y, Coordinate parent, int priority) {
+    Node(int x, int y, Node parent, int pathCost) {
         this.x = x;
         this.y = y;
         this.parent = parent;
-        this.priority = priority;
+        this.pathCost = pathCost;
+    }
+
+    Node(int x, int y, Node parent, int gCost, int hCost) {
+        this.x = x;
+        this.y = y;
+        this.parent = parent;
+        this.pathCost = gCost + hCost;
+        this.gCost = gCost;
+        this.hCost = hCost;
     }
 
     public String toString(){
@@ -30,14 +41,27 @@ class Coordinate implements Comparable<Coordinate> {
     }
 
     @Override
-    public int compareTo(Coordinate o) {
-        if(this.priority > o.priority) {
+    public int compareTo(Node o) {
+        if(this.pathCost > o.pathCost) {
             return 1;
-        } else if (this.priority < o.priority) {
+        } else if (this.pathCost < o.pathCost) {
             return -1;
         } else {
             return 0;
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return x == node.x && y == node.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
 
@@ -50,47 +74,51 @@ public class homework {
     public static void main(String[] args) {
         Scanner scanner = null;
         try {
-            scanner = new Scanner(new File("src/work/input6.txt"));
+            scanner = new Scanner(new File("src/work/input8.txt"));
             //scanner = new Scanner(new File("input.txt"));
             String typeOfAlgorithm = scanner.nextLine();
 
             String[] dimensions = scanner.nextLine().split(" ");
             int columnWidth = Integer.parseInt(dimensions[0]);
             int rowHeight = Integer.parseInt(dimensions[1]);
-            int matrix[][] = new int[rowHeight][columnWidth];
+            int grid[][] = new int[rowHeight][columnWidth];
 
             String[] startingPos = scanner.nextLine().split(" ");
-            Coordinate startingCoord = new Coordinate(Integer.parseInt(startingPos[0]), Integer.parseInt(startingPos[1]), null);
+            Node startingCoord = new Node(Integer.parseInt(startingPos[0]), Integer.parseInt(startingPos[1]), null);
 
             int maxRockHeight = Integer.parseInt(scanner.nextLine());
 
             int noOfSites = Integer.parseInt(scanner.nextLine());
-            Map<String, Coordinate> allSitesPaths = new LinkedHashMap<>();
+            Map<String, Node> allDestinations = new LinkedHashMap<>();
 
             for(int i=0; i<noOfSites; i++) {
                 String[] site = scanner.nextLine().split(" ");
-                Coordinate siteCoord = new Coordinate(Integer.parseInt(site[0]), Integer.parseInt(site[1]), null);
-                allSitesPaths.put(siteCoord.toString(), new Coordinate(siteCoord.x, siteCoord.y, null));
+                Node siteCoord = new Node(Integer.parseInt(site[0]), Integer.parseInt(site[1]), null);
+                allDestinations.put(siteCoord.toString(), new Node(siteCoord.x, siteCoord.y, null));
             }
 
             for(int i=0; i<rowHeight; i++) {
                 String[] nextRow = scanner.nextLine().split(" ");
                 for(int j=0; j<columnWidth; j++) {
-                    matrix[i][j] = Integer.parseInt(nextRow[j]);
+                    grid[i][j] = Integer.parseInt(nextRow[j]);
                 }
             }
 
             switch(typeOfAlgorithm) {
                 case BFS:
-                    solveUsingBFS(columnWidth, rowHeight, startingCoord, maxRockHeight, noOfSites, allSitesPaths, matrix);
+                    solveUsingBFS(columnWidth, rowHeight, startingCoord, maxRockHeight, noOfSites, allDestinations, grid);
                     break;
 
                 case UCS:
-                    solveUsingUCS(columnWidth, rowHeight, startingCoord, maxRockHeight, noOfSites, allSitesPaths, matrix);
+                    solveUsingUCS(columnWidth, rowHeight, startingCoord, maxRockHeight, noOfSites, allDestinations, grid);
                     break;
 
                 case A_STAR:
-                    solveUsingAStar(columnWidth, rowHeight, startingCoord, maxRockHeight, noOfSites, allSitesPaths, matrix);
+                    for (Map.Entry<String, Node> entry : allDestinations.entrySet()) {
+                        Node result = getResultUsingAStar(columnWidth, rowHeight, startingCoord, maxRockHeight, grid, entry.getValue());
+                        allDestinations.put(entry.getKey(), result);
+                    }
+                    addToOutputFile(allDestinations);
                     break;
             }
         } catch (Exception e) {
@@ -98,9 +126,9 @@ public class homework {
         }
     }
 
-    private static void solveUsingBFS(int columnWidth, int rowHeight, Coordinate startingCoord, int maxRockHeight,
-                                      int noOfSites, Map<String, Coordinate> allSitesPaths, int[][] matrix) {
-        Queue<Coordinate> queue = new LinkedList<>();
+    private static void solveUsingBFS(int columnWidth, int rowHeight, Node startingCoord, int maxRockHeight,
+                                      int noOfSites, Map<String, Node> allSitesPaths, int[][] grid) {
+        Queue<Node> queue = new LinkedList<>();
         boolean closed[][] = new boolean[rowHeight][columnWidth];
         closed[startingCoord.y][startingCoord.x] = true;
         queue.add(startingCoord);
@@ -108,7 +136,7 @@ public class homework {
         int noOfPendingSites = noOfSites;
 
         while(!queue.isEmpty() && noOfPendingSites  != 0) {
-            Coordinate currentCoord = queue.peek();
+            Node currentCoord = queue.peek();
             if(allSitesPaths.get(currentCoord.toString()) != null) {
                 noOfPendingSites--;
                 allSitesPaths.put(currentCoord.toString(), currentCoord);
@@ -116,27 +144,27 @@ public class homework {
 
             queue.poll();
 
-            List<Coordinate> allNeighbors = getAllUnclosedNeighboursBFS(matrix, currentCoord, columnWidth, rowHeight, maxRockHeight, closed);
+            List<Node> allNeighbors = getAllUnclosedNeighboursBFS(grid, currentCoord, columnWidth, rowHeight, maxRockHeight, closed);
             if (!allNeighbors.isEmpty()) {
                 queue.addAll(allNeighbors);
             }
         }
 
-        printAllSitesPaths(allSitesPaths);
+        addToOutputFile(allSitesPaths);
     }
 
 
 
-    private static List<Coordinate> getAllUnclosedNeighboursBFS(int[][] matrix, Coordinate currentCoord, int columnWidth, int rowHeight,
-                                                                 int maxRockHeight, boolean[][] closed) {
-        int currentHeight = matrix[currentCoord.y][currentCoord.x] < 0 ? Math.abs(matrix[currentCoord.y][currentCoord.x]) : 0;
-        List<Coordinate> neighbours = new ArrayList<>();
+    private static List<Node> getAllUnclosedNeighboursBFS(int[][] grid, Node currentCoord, int columnWidth, int rowHeight,
+                                                          int maxRockHeight, boolean[][] closed) {
+        int currentHeight = grid[currentCoord.y][currentCoord.x] < 0 ? Math.abs(grid[currentCoord.y][currentCoord.x]) : 0;
+        List<Node> neighbours = new ArrayList<>();
 
         for(int y = Math.max(0, currentCoord.y-1); y <= Math.min(currentCoord.y+1, rowHeight-1); y++){
                 for(int x = Math.max(0, currentCoord.x-1); x <= Math.min(currentCoord.x+1, columnWidth-1); x++){
-                    Coordinate neighbour = new Coordinate(x,y, currentCoord);
+                    Node neighbour = new Node(x,y, currentCoord);
                     if((neighbour.x != currentCoord.x || neighbour.y != currentCoord.y) && !closed[y][x]){
-                        int height = matrix[y][x] < 0 ? Math.abs(matrix[y][x]) : 0;
+                        int height = grid[y][x] < 0 ? Math.abs(grid[y][x]) : 0;
                         if (Math.abs(currentHeight - height) <= maxRockHeight) {
                             neighbours.add(neighbour);
                             closed[neighbour.y][neighbour.x] = true;
@@ -147,59 +175,142 @@ public class homework {
         return neighbours;
     }
 
-    private static void solveUsingUCS(int columnWidth, int rowHeight, Coordinate startingCoord, int maxRockHeight,
-                                      int noOfSites, Map<String, Coordinate> allSitesPaths, int[][] matrix) {
-        PriorityQueue<Coordinate> ucsQueue = new PriorityQueue<>();
-        boolean closed[][] = new boolean[rowHeight][columnWidth];
-        closed[startingCoord.y][startingCoord.x] = true;
-        ucsQueue.add(startingCoord);
+    private static void solveUsingUCS(int columnWidth, int rowHeight, Node startingCoord, int maxRockHeight,
+                                      int noOfSites, Map<String, Node> allSitesPaths, int[][] grid) {
+        PriorityQueue<Node> ucsOpenQueue = new PriorityQueue<>();
+        Map<String, Integer> costMap = new HashMap<>();
+        Set<Node> closed = new HashSet<>();
+        ucsOpenQueue.add(startingCoord);
         int distance = 0;
+        costMap.put(startingCoord.toString(), 0);
 
         int noOfPendingSites = noOfSites;
 
-        while(!ucsQueue.isEmpty() && noOfPendingSites  != 0) {
-            Coordinate currentCoord = ucsQueue.peek();
-            if(allSitesPaths.get(currentCoord.toString()) != null) {
+        while(!ucsOpenQueue.isEmpty() && noOfPendingSites  != 0) {
+            Node currentNode = ucsOpenQueue.poll();
+            closed.add(currentNode);
+            if(allSitesPaths.get(currentNode.toString()) != null) {
+                System.out.println("Goal reached " + currentNode.toString());
                 noOfPendingSites--;
-                allSitesPaths.put(currentCoord.toString(), currentCoord);
+                allSitesPaths.put(currentNode.toString(), currentNode);
             }
 
-            ucsQueue.poll();
-            int currentHeight = matrix[currentCoord.y][currentCoord.x] < 0 ? Math.abs(matrix[currentCoord.y][currentCoord.x]) : 0;
+            int currentHeight = grid[currentNode.y][currentNode.x] < 0 ? Math.abs(grid[currentNode.y][currentNode.x]) : 0;
 
-            for(int y = Math.max(0, currentCoord.y-1); y <= Math.min(currentCoord.y+1, rowHeight-1); y++){
-                for(int x = Math.max(0, currentCoord.x-1); x <= Math.min(currentCoord.x+1, columnWidth-1); x++){
-                    if((x != currentCoord.x || y != currentCoord.y) && !closed[y][x]){
-                        int height = matrix[y][x] < 0 ? Math.abs(matrix[y][x]) : 0;
+            for(int y = Math.max(0, currentNode.y-1); y <= Math.min(currentNode.y+1, rowHeight-1); y++){
+                for(int x = Math.max(0, currentNode.x-1); x <= Math.min(currentNode.x+1, columnWidth-1); x++){
+                    //not the same node as current
+                    if(x != currentNode.x || y != currentNode.y){
+                        int height = grid[y][x] < 0 ? Math.abs(grid[y][x]) : 0;
                         if (Math.abs(currentHeight - height) <= maxRockHeight) {
-                            distance = (x != currentCoord.x && y != currentCoord.y) ? 14 : 10;
-                            Coordinate neighbour = new Coordinate(x,y, currentCoord, currentCoord.priority + distance);
-                            ucsQueue.add(neighbour);
-                            closed[neighbour.y][neighbour.x] = true;
+                            distance = (x != currentNode.x && y != currentNode.y) ? 14 : 10;
+                            Node child = new Node(x,y, currentNode, currentNode.pathCost + distance);
+                            //add node to queue if node has not been explored
+                            if (!closed.contains(child) && !ucsOpenQueue.contains(child)) {
+                                ucsOpenQueue.add(child);
+                                costMap.put(child.toString(), child.pathCost);
+                            }
+                            //current path is shorter than previous path found
+                            else if(ucsOpenQueue.contains(child) && child.pathCost < costMap.get(child.toString())) {
+                                ucsOpenQueue.remove(child);
+                                ucsOpenQueue.add(child);
+                                costMap.put(child.toString(), child.pathCost);
+                            }
+                            else if(closed.contains(child) && child.pathCost < costMap.get(child.toString())) {
+                                closed.remove(child);
+                                ucsOpenQueue.add(child);
+                                costMap.put(child.toString(), child.pathCost);
+                            }
                         }
                     }
                 }
             }
         }
 
-        printAllSitesPaths(allSitesPaths);
+        addToOutputFile(allSitesPaths);
     }
 
-    private static void solveUsingAStar(int columnWidth, int rowHeight, Coordinate startingCoord, int maxRockHeight,
-                                      int noOfSites, Map<String, Coordinate> allSitesPaths, int matrix[][]) {
-        System.out.println("A* to be implemented");
+    private static Node getResultUsingAStar(int columnWidth, int rowHeight, Node startingCoord, int maxRockHeight,
+                                            int grid[][], Node goal) {
+        PriorityQueue<Node> aStarOpenQueue = new PriorityQueue<>();
+        Map<String, Integer> costMap = new HashMap<>();
+        Set<Node> closed = new HashSet<>();
+        aStarOpenQueue.add(startingCoord);
+        int distance = 0;
+        costMap.put(startingCoord.toString(), 0);
+
+        while(!aStarOpenQueue.isEmpty()) {
+            Node currentNode = aStarOpenQueue.poll();
+            closed.add(currentNode);
+            if(currentNode.x == goal.x && currentNode.y == goal.y) {
+                System.out.println("Goal reached " + currentNode.toString());
+                return currentNode;
+            }
+
+            int currentHeight = grid[currentNode.y][currentNode.x] < 0 ? Math.abs(grid[currentNode.y][currentNode.x]) : 0;
+
+            int gCost, hCost;
+            for(int y = Math.max(0, currentNode.y-1); y <= Math.min(currentNode.y+1, rowHeight-1); y++){
+                for(int x = Math.max(0, currentNode.x-1); x <= Math.min(currentNode.x+1, columnWidth-1); x++){
+                    //not the same node as current
+                    if(x != currentNode.x || y != currentNode.y){
+                        int height = grid[y][x] < 0 ? Math.abs(grid[y][x]) : 0;
+                        if (Math.abs(currentHeight - height) <= maxRockHeight) {
+                            // movement cost
+                            gCost = (x != currentNode.x && y != currentNode.y) ? 14 : 10;
+                            
+                            //muddiness cost
+                            gCost += grid[y][x] > 0 ? Math.abs(grid[y][x]) : 0;
+
+                            //height cost
+                            gCost += Math.abs(currentHeight - height);
+
+                            //distance till parent
+                            gCost += currentNode.gCost;
+
+                            //heuristic cost
+                            hCost = Math.max(Math.abs(x - goal.x), Math.abs(y - goal.y));
+
+                            distance = gCost + hCost;
+                            Node child = new Node(x,y, currentNode, gCost, hCost);
+
+                            //add node to queue if node has not been explored
+                            if (!closed.contains(child) && !aStarOpenQueue.contains(child)) {
+                                aStarOpenQueue.add(child);
+                                costMap.put(child.toString(), distance);
+                            }
+                            //current path is shorter than previous path found
+                            else if(aStarOpenQueue.contains(child) && distance < costMap.get(child.toString())) {
+                                System.out.println("Replacing cost as better found");
+                                aStarOpenQueue.remove(child);
+                                aStarOpenQueue.add(child);
+                                costMap.put(child.toString(), child.pathCost);
+                            }
+                            else if(closed.contains(child) && distance < costMap.get(child.toString())) {
+                                System.out.println("Replacing cost as better found");
+                                closed.remove(child);
+                                aStarOpenQueue.add(child);
+                                costMap.put(child.toString(), child.pathCost);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return null;
     }
 
-    private static void printAllSitesPaths(Map<String, Coordinate> allSitesPaths) {
+    private static void addToOutputFile(Map<String, Node> allSitesPaths) {
 
         try {
             FileWriter fw = new FileWriter("output.txt");
 
-            for (Map.Entry<String, Coordinate> entry : allSitesPaths.entrySet()) {
+            for (Map.Entry<String, Node> entry : allSitesPaths.entrySet()) {
+                System.out.println("path cost " + entry.getValue().pathCost);
                 if (entry.getValue() == null || entry.getValue().parent == null) {
                     fw.write("FAIL");
                 } else {
-                    Coordinate currentCoord = entry.getValue();
+                    Node currentCoord = entry.getValue();
                     StringBuilder sbr = new StringBuilder();
                     while (currentCoord != null) {
                         sbr.insert(0, currentCoord.x + "," + currentCoord.y + " ");
